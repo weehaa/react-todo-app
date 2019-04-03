6c917731cdf4ba24634e490bdd51e2677d3f8ae2
weehaa <vzg1000@mail.ru>
2019-04-04 00:17:00 +0400

    vid036 - add  component state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,23 +3,39 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    onLabelClick = () => console.log(`Got click on ${this.props.label}`);
+    state = {
+        done: false
+    };
+
+    onLabelClick = () => {
+        // console.log(`Got click on ${this.props.label}`);
+        this.setState({
+            done: !this.state.done
+        });
+    };
 
     render() {
 
         const  { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
 
         const style = {
-            color: important ? 'tomato' : 'black'
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal',
         };
 
         return (
-            <div className="todo-list-item">
+            <div className={ classNames }>
                 <button className="btn btn-outline-success float-left" type="button" >
                     <i className="fa fa-exclamation" />
                 </button>
                 <span
-                    className="col-10 todo-list-item"
+                    className="todo-list-item-label"
                     style= {style}
                     onClick={ this.onLabelClick }>
                     { label }
