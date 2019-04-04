8d8f4f29caa7fe440bbe9b3766af563fb22f92bb
weehaa <vzg1000@mail.ru>
2019-04-04 20:48:19 +0400

    vid044 submit form for new item

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -4,24 +4,41 @@ import './add-item.css';
 
 export default class AddItem extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        // new state doesn't depend on previous
+        this.setState({
+            label: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
+        // default submit is reloading page!
+        e.preventDefault();
+        this.props.onAddItem(this.state.label)
+    };
+
     render() {
-        const { onAddItem } = this.props;
         return (
-            <div className="input-group mb-3 mt-2 add-item">
+            <form className="input-group mb-3 mt-2 add-item"
+                  onSubmit={this.onSubmit}>
                 <input type="text"
                        className="form-control"
+                       onChange={this.onLabelChange}
                        placeholder="new item"
                        aria-label="new item"
                        aria-describedby="button-addon2" />
                     <div className="input-group-append">
                         <button className="btn btn-outline-secondary"
-                                type="button"
-                                id="button-addon2"
-                                onClick={() => onAddItem('aaa')}>
+                                type="submit"
+                                id="button-addon2">
                             Add
                         </button>
                     </div>
-            </div>
+            </form>
         );
     };
 }
